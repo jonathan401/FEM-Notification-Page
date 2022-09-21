3a794826b86303f08b24f89d6cc3e6702d48f895
jonathan401 <jonathangomina5049@gmail.com>
2022-09-21 16:50:27 +0100

    improve theme toggle accessibility

--- src/components/common/ThemeToggle.tsx
+++ src/components/common/ThemeToggle.tsx
@@ -5,11 +5,12 @@ import ThemeContext, { ThemeContextType } from "../../contexts/ThemeContext";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 
 export const ThemeToggleStyled = styled.button`
-  margin: 0.6rem 0.6rem 0.6rem auto;
+  margin: 0.5rem 0 1.3rem auto;
   border-radius: 0.4rem;
-  border: 0;
-  padding: 0.4rem 0.5rem;
+  padding: 0.4rem 0.3rem;
   outline: 0;
+  border: 0;
+  width: fit-content;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -21,17 +22,31 @@ export const ThemeToggleStyled = styled.button`
     outline-offset: 2px;
     outline: 2px solid ${({ theme }) => theme.tertiary};
   }
+
+  span {
+    font-size: clamp(0.87rem, 2vw, 0.95rem);
+    padding: 0 0.5rem;
+    color: ${({ theme }) => theme.textLight};
+    display: inline-block;
+  }
 `;
 
 const ThemeToggle = () => {
   const { theme, switchTheme } = useContext(ThemeContext) as ThemeContextType;
   return (
-    <ThemeToggleStyled onClick={() => switchTheme(theme)}>
-      {theme === "dark" ? (
-        <MdLightMode size="20" color="#def3fa" />
+    <ThemeToggleStyled
+      type="button"
+      aria-label="theme switch"
+      role="switch"
+      aria-checked={theme === "dark" ? true : false}
+      onClick={() => switchTheme(theme)}
+    >
+      {theme === "light" ? (
+        <MdLightMode aria-hidden="true" size="20" color="#def3fa" />
       ) : (
-        <MdDarkMode size="20" color="#def3fa" />
+        <MdDarkMode aria-hidden="true" size="20" color="#def3fa" />
       )}
+      <span>{theme === "dark" ? "on" : "off"}</span>
     </ThemeToggleStyled>
   );
 };
